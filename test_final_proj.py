0824e9549bb808b9599fa5c9a08f5b1cd76813e2
Nikhar Shah <nikharshah@Nikhars-MacBook-Pro.local>
2021-05-01 16:10:58 -0400

    travis test added

---
+++ test_final_proj.py
@@ -0,0 +1,13 @@
+
+from unittest import TestCase
+
+
+class Tests_final(TestCase):
+
+    def test_fibonnacci(self):
+        """
+
+        Returns: random test true or not
+
+        """
+        self.assertEqual(40, 40)
